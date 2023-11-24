77f874286387dff0f11d63c0da8ec77468e03d34
Marcos Vinícius Pedreira Vieira <viniciuspv.si@gmail.com>
2023-11-24 15:16:55 -0300

    feat: dashboard page with Next Js

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return(
+    <h1>customers page</h1>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+export default function Page(){
+    return(
+        <h1>Dashboard Page</h1>
+    );
+
+}
